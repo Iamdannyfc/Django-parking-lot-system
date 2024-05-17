d293956fc547a15b9b208b639b045b3da18fdf92
Iamdannyfc <32012003+Iamdannyfc@users.noreply.github.com>
2024-05-17 13:29:56 +0100

    Update admin.py

--- parkinglot/admin.py
+++ parkinglot/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ParkingLot, Floor, Vehicle, ParkingSlot
 
-# Register your models here.
+admin.site.register(ParkingLot)
+admin.site.register(Floor)
+admin.site.register(Vehicle)
+admin.site.register(ParkingSlot)
