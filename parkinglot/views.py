c7a12e3bd71ff70a12a2aa72482b6e44e6bd7a45
Iamdannyfc <32012003+Iamdannyfc@users.noreply.github.com>
2024-05-22 16:56:24 +0100

    Update views.py

--- parkinglot/views.py
+++ parkinglot/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import ParkingLot
+from .serializers import ParkingLotSerializer
 
-# Create your views here.
+class ParkingLotCreateView(generics.CreateAPIView):
+    queryset = ParkingLot.objects.all()
+    serializer_class = ParkingLotSerializer
