4264bc601b32d3fbcff9eadcf820cbc1f33b2849
Iamdannyfc <32012003+Iamdannyfc@users.noreply.github.com>
2024-05-22 17:47:18 +0100

    Update urls.py

--- parkinglot/urls.py
+++ parkinglot/urls.py
@@ -3,5 +3,5 @@ from .views import ParkingLotCreateView, park_vehicle
 
 urlpatterns = [
     path('create-parking-lot/', ParkingLotCreateView.as_view(), name='create-parking-lot'),
-    path('park-vehicle/', park_vehicle, name='park-vehicle'),
+    path('park-vehicle/', ParkVehicleView.as_view(), name='park-vehicle'),
 ]
